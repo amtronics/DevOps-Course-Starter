4c02502ac7e29167f4f4198a4387dddacc5132b8
gha1yok <amin.gholamzadeh@etas.com>
2022-11-30 15:03:12 +0000

    Improve styling & add mark as complete function

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, get_item, add_item, save_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -12,10 +12,18 @@ def index():
     todos = get_items()
     return render_template('index.html', todos=todos)
 
-@app.route('/create', methods=['POST'])
-def create():
-    ''' for creating new to-do items via the form in index '''
+@app.route('/update', methods=['POST'])
+def update():
+    ''' for creating new to-do items via the form in index or marking them as complete'''
+    completed_ids = request.form.getlist('completedCheck')
+    for id in completed_ids:
+        item = get_item(id)
+        item['status'] = 'Completed'
+        save_item(item)
+
     field_name = request.form.get('field_name')
-    print(request.form)
-    add_item(field_name)
+    if field_name:
+        # brief sanity check
+        add_item(field_name)
+
     return redirect(url_for('index'))
