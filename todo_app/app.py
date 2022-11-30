51189d925cbf3f157de8d071f75acda3728029f0
gha1yok <amin.gholamzadeh@etas.com>
2022-11-30 13:38:00 +0000

    Create & Display to-do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +8,14 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    ''' landing page '''
+    todos = get_items()
+    return render_template('index.html', todos=todos)
+
+@app.route('/create', methods=['POST'])
+def create():
+    ''' for creating new to-do items via the form in index '''
+    field_name = request.form.get('field_name')
+    print(request.form)
+    add_item(field_name)
+    return redirect(url_for('index'))
