ab09b6cd50de9dc92fb2561ec366882981ebe311
amtronics <amin.xero@gmail.com>
2023-02-06 22:15:47 +0000

    Implement Add/Update

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,48 +1,84 @@
 from functools import cached_property
+from urllib3.exceptions import InsecureRequestWarning
+import typing
 import requests
-import json
 import os
 
 
-class TrelloItems():
+# Suppress the warnings from urllib3
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+class Item:
+    ''' Class to represent an item (card)'''
+    def __init__(self, id, name, status='To Do'):
+        self.id = id
+        self.name = name
+        self.status = status
+
+    @classmethod
+    def from_trello_card(cls, card, list):
+        return cls(card['id'], card['name'], list['name'])
+
+
+class TrelloList:
+    ''' Class to represent a List'''
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    @classmethod
+    def from_trello_list(cls, list):
+        return cls(list['id'], list['name'])
+
+
+class TrelloAPI():
     ''' Service Trello API related actions '''
     def __init__(self) -> None:
         self.API_KEY = os.environ.get('API_KEY')
         self.API_TOKEN = os.environ.get('API_TOKEN')
         self.BOARD_ID = os.environ.get('BOARD_ID')
+        self.cards : typing.List[Item] = []
+        self.lists : typing.List[TrelloList] = []
 
-    def _request_lists(self):
-        ''' Get Filtered Lists on a Board '''
-        url = f"https://api.trello.com/1/boards/{self.BOARD_ID}/lists"
+        # call get_items to initialise in RAM cards and lists properties
+        self.get_items()
+
+    def _send_requst(self, url, method='GET', **query):
+        '''
+        Send a request to Trello API
+
+        Args:
+            url: url after https://api.trello.com/1/
+            query: kwargs for HTML query, excluding API Key & Token
+
+        Returns:
+            JSON response
+        '''
+        url = f"https://api.trello.com/1/{url}"
         headers = {"Accept": "application/json"}
-        query = {"key": self.API_KEY,
-                 "token": self.API_TOKEN,
-                 "cards": "open",
-                 "card_fields": "id,name,idList"}
-        response = requests.request("GET", url, headers=headers, params=query, verify=False)
+        _query = {"key": self.API_KEY,
+                  "token": self.API_TOKEN}
+        _query.update(query)
+        response = requests.request(method, url, headers=headers, params=_query, verify=False)
         return response.json()
-
-    @cached_property
-    def _status_idlist_mapping(self):
-        mapping = {}
-        for trello_list in self._request_lists():
-            mapping[trello_list['id']] = trello_list['name']
-        return mapping
 
     def get_items(self):
         """
-        Fetches all saved items from the Trello.
+        Fetches all saved items from Trello.
 
         Returns:
             list: The list of saved items.
         """
-        cards = []
-        for trello_list in self._request_lists():
+        self.cards = []
+        self.lists = []
+        url = f"boards/{self.BOARD_ID}/lists"
+        query = {"cards": "open", "card_fields": "id,name,idList"}
+        for trello_list in self._send_requst(url, **query):
             for card in trello_list['cards']:
-                cards.append({'id': card['id'],
-                              'status': self._status_idlist_mapping[card['idList']],
-                              'title': card['name']})
-        return cards
+                self.cards.append(Item.from_trello_card(card, trello_list))
+            self.lists.append(TrelloList.from_trello_list(trello_list))
+        return self.cards
 
     def get_item(self, id):
         """
@@ -54,42 +90,29 @@ class TrelloItems():
         Returns:
             item: The saved item, or None if no items match the specified ID.
         """
-        items = self.get_items()
+        items = self.cards
         return next((item for item in items if item['id'] == int(id)), None)
 
-    # def add_item(self, title):
-    #     """
-    #     Adds a new item with the specified title to the Trello.
+    def save_item(self, id, new_status):
+        """
+        Updates an existing item in the Trello. If no existing item matches the ID of the specified item, nothing is saved.
 
-    #     Args:
-    #         title: The title of the item.
+        Args:
+            id: The id of item to change
+            new_status: new status of the item
+        """
+        lst_id = [lst.id for lst in self.lists if lst.name == new_status][0]
+        url = f"cards/{id}"
+        query = {"idList": lst_id}
+        self._send_requst(url, 'PUT', **query)
 
-    #     Returns:
-    #         item: The saved item.
-    #     """
-    #     items = self.get_items()
+    def add_item(self, name):
+        """
+        Adds a new item with the specified name to the Trello.
 
-    #     # Determine the ID for the item based on that of the previously added item
-    #     id = items[-1]['id'] + 1 if items else 0
-
-    #     item = { 'id': id, 'title': title, 'status': 'Not Started' }
-
-    #     # Add the item to the list
-    #     items.append(item)
-    #     # session['items'] = items
-
-    #     return item
-
-    # def save_item(self, item):
-    #     """
-    #     Updates an existing item in the Trello. If no existing item matches the ID of the specified item, nothing is saved.
-
-    #     Args:
-    #         item: The item to save.
-    #     """
-    #     existing_items = self.get_items()
-    #     updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
-    #     # session['items'] = updated_items
-
-    #     return item
+        Args:
+            name: The name of the item.
+        """
+        lst_id = [lst.id for lst in self.lists if lst.name == "To Do"][0]
+        query = {"idList": lst_id, "name": name}
+        self._send_requst("cards/", 'POST', **query)
